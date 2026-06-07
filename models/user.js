251cc9a50ad11a3b7fdf57d191d04b10df7acc2e
VarunSuddala <varunsuddala446@gmail.com>
2026-06-07 22:51:08 +0530

    first commit

---
+++ models/user.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+
+
+const userSchema = new mongoose.Schema({
+
+    name: {
+        type: String,
+        required: [true, "name must be filled"]
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: [true, "email must be unique"],
+        lowercase: true,
+
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: [8, "must be 8 characters"]
+    },
+
+    role: {
+        type: String,
+        enum: ["student", "tpo", "admin", "mentor", "trainer"],
+        default: "student"
+    },
+
+    refreshToken: {
+        type: String,
+        required: false,
+        select: false
+    },
+
+    createdAt: {
+        type: Date,
+        default: Date.now(),
+    },
+
+    updatedAt: {
+        type: Date,
+        default: Date.now(),
+    }
+});
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+const PORT = 3000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+app.get("/", (req, res) => {
+    res.send("Hello , wellcome to talentBridge API");
+});
+

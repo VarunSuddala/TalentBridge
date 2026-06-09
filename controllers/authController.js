948b0e7c927b7910898bd8e83c2a3405325f683e
VarunSuddala <varunsuddala446@gmail.com>
2026-06-10 00:08:16 +0530

    feat: addded auth controllers and ratelimit for login

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,5 @@
 const bcrypt = require("bcryptjs");
-
+const User = require("../models/user");
 
 function HashPassword(password) {
     const salt = bcrypt.genSaltSync(10);
@@ -12,7 +12,37 @@ function comparePassword(password, hashpassword) {
     return bcrypt.compareSync(password, hashpassword);
 }
 
+async function register(userDetails) {
+    userDetails.password = await HashPassword(userDetails.password);
+    try {
+        await User.create(userDetails);
+        console.log("user registered successfully");
+        return { status: true, msg: "user registered successfully" };
+    }
+    catch (err) {
+        console.log("error in registration", err.message);
+        return { status: false, msg: err.message };
+    }
+};
+
+async function login(email, password) {
+    const u = await User.findOne({ email });
+    if (!u) {
+        return { status: false, msg: "email Not Found" }
+    };
+
+    const ok = comparePassword(password, u.password);
+
+    if (!ok) {
+        return { status: false, msg: "Incorrect Password" }
+    };
+
+    return { status: true, user: u, msg: "loging sucessfully" }
+
+};
 module.exports = {
     HashPassword,
-    comparePassword
+    comparePassword,
+    register,
+    login
 };
--- routes/authroutes.js
+++ routes/authroutes.js
@@ -1,24 +1,46 @@
 const express = require("express");
 const router = express.Router();
-const { HashPassword, comparePassword } = require("../controllers/authController.js");
+const { register, login } = require("../controllers/authController.js");
+const User = require("../models/user.js");
 
 router.get("/", (req, res) => {
     res.send("Auth is working");
 });
 
-router.post("/register", (req, res) => {
+router.post("/register", async (req, res) => {
 
     const { name, email, password, role } = req.body;
 
     const user = {
         "name": name,
         "email": email,
-        "password": HashPassword(password),
+        "password": password,
         "role": role
     };
-    res.json({
-        "msg": "registration successfull",
-        "user": user
+    const result = await register(user);
+    if (!result.status) {
+        return res.status(400).json({ msg: result.msg })
+    }
+    res.status(201).json({ msg: result.msg });
+
+});
+
+router.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+    const result = await login(email, password);
+
+    if (!result.status) {
+        return res.status(404).json({ msg: result.msg });
+    }
+    res.status(200).json({ msg: result.msg, user: result.user });
+
+});
+
+router.post("/logout", async (req, res) => {
+
+    req.session.destroy(() => {
+        res.clearCookie('connect.sid');
+        res.json({ message: 'Logged out' });
     });
 
 });
